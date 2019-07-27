68c243bc124d2edd2e654315fba385b6e30ba24d
linh803 <phamphuonglinh2303@gmail.com>
2019-07-28 02:36:07 +1000

    Style contribute.html

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="./nav.css">
         <link rel="stylesheet" href="./style.css">
-        <link rel="stylesheet" href="./index.css">
+        <link rel="stylesheet" href="./background.css">
     </head>
 
-    <body id="index">
+    <body id="bckgrd" background="./images/home-background.png">
         <nav class="navbar navbar-expand-md justify-content-end">
             <ul class="navbar-nav">
                 <li class="nav-item">
